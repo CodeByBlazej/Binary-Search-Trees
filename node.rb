5cef1ac3ebc6ce2486c916fb747f9809c1ff7ff3
CodeByBlazej <codebyblazej@gmail.com>
2024-12-07 14:23:17 +0000

    add new method

--- node.rb
+++ node.rb
@@ -1,10 +1,26 @@
 class Node
   attr_accessor :data, :left, :right
 
-  def initialize(data, left, right)
+  def initialize(data)
     @data = data
     @left = left
     @right = right
   end
+
+  def sorted_arr_to_BST(arr, first, last)
+    return nil if first > last
+
+    mid = first + (last - first) / 2
+
+    root = Node.new(arr[mid])
+
+    root.left = sorted_arr_to_BST(arr, first, mid - 1)
+
+    root.right = sorted_arr_to_BST(arr, mid + 1, last)
+
+    return root
+  end
+
+  def sorted
 
 end
