7566298e947ec749750eaa263a7b0afeaaeb563f
CodeByBlazej <codebyblazej@gmail.com>
2024-12-08 20:54:46 +0000

    will test insert node

--- main.rb
+++ main.rb
@@ -5,3 +5,9 @@ tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 tree.build_tree
 
 tree.pretty_print
+
+tree.insert_node(6)
+tree.pretty_print
+
+tree.insert_node(4568)
+tree.pretty_print
