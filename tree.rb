8d6824eda25d65bcf9ed3d63719b497f12caa2d8
CodeByBlazej <codebyblazej@gmail.com>
2024-12-08 12:52:49 +0000

    add pretty print method

--- tree.rb
+++ tree.rb
@@ -14,7 +14,13 @@ class Tree
     
     @root = new_node.sorted_array_to_BST(sorted_arr)
     new_node.pre_order(@root)
-    
+
     puts "\nroot of this tree is: #{@root.data}"
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
