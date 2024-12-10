dfb4cf09f3f1b461be665316834ec2995034bddd
CodeByBlazej <codebyblazej@gmail.com>
2024-12-10 18:27:53 +0000

    add delete method

--- tree.rb
+++ tree.rb
@@ -24,6 +24,11 @@ class Tree
     @root.pre_order(@root)
   end
 
+  def delete(data)
+    @root.del_node(@root, data)
+    
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
